c74e342bf2beed715b4dc8d287037f0e50c4d59b
HyperLife1119 <hyperlife1119@qq.com>
2024-08-30 15:55:23 +0800

    refactor(core): update some command patterns

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -3,18 +3,18 @@ export const LOW_BATTERY = 20;
 
 /** 通用 */
 export const ATT_CMD_PATTERN = /^ATT\+\w+=.+/;
-/** ATT+WGT=-28,1721251,0|1    1代表稳定 */
-export const ATT_WGT_PATTERN = /^ATT\+WGT=-?\d+,-?\d+,\d$/;
+/** ATT+WGT=-1.23,1721251,0|1    1代表稳定 */
+export const ATT_WGT_PATTERN = /^ATT\+WGT=-?\d+(\.\d+)?,-?\d+,\d$/;
 /** ATT+ACK=1 */
 export const ATT_ACK_PATTERN = /^ATT\+ACK=[01]$/;
 /** ATT+BAT=99,0 */
 export const ATT_BAT_PATTERN = /^ATT\+BAT=\d{1,3},[01]$/;
 /** ATT+IDLE=10 */
 export const ATT_IDLE_PATTERN = /^ATT\+IDLE=\d+$/;
-/** ATT+LOCK=10 */
-export const ATT_LOCK_PATTERN = /^ATT\+LOCK=\d+$/;
-/** ATT+ACCURACY=1000 */
-export const ATT_ACCURACY_PATTERN = /^ATT\+ACCURACY=\d+$/;
+/** ATT+LOCK=1.23 */
+export const ATT_LOCK_PATTERN = /^ATT\+LOCK=\d+(\.\d+)?$/;
+/** ATT+ACCURACY=1.23 */
+export const ATT_ACCURACY_PATTERN = /^ATT\+ACCURACY=\d+(\.\d+)?$/;
 /** ATT+AUTO_CLOSE=5 */
 export const ATT_AUTO_CLOSE_PATTERN = /^ATT\+AUTO_CLOSE=\d+$/;
 /** ATT+FILTER=0 */
