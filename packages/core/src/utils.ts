db1b385d97dc7ef10f3bd8a881fafb58e7a0e62e
HyperLife1119 <hyperlife1119@qq.com>
2026-08-06 19:15:12 +0800

    fix(core): update isOpperDevice function to use a more generic device type

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,6 +1,5 @@
 import { Attribute, AttributeCommand } from './attribute';
 import { ATT_ACCURACY_PATTERN, ATT_ACK_PATTERN, ATT_AUTO_CLOSE_PATTERN, ATT_BAT_PATTERN, ATT_CMD_PATTERN, ATT_FILTER_PATTERN, ATT_IDLE_PATTERN, ATT_LOCK_PATTERN, ATT_WGT_PATTERN } from './constants';
-import { BluetoothDevice } from './typing';
 
 const ATTRIBUTE_TOKEN_PREFIX = 'ATT+' as const;
 const OPPER_NAME_PATTERN = /^OPPER-M\d+\s\w{4}$/;
@@ -67,7 +66,7 @@ export function parseAttributeCommand(cmd: string): AttributeCommand {
   };
 }
 
-export function isOpperDevice(device: BluetoothDevice): boolean {
+export function isOpperDevice(device: { name: string }): boolean {
   return device.name ? OPPER_NAME_PATTERN.test(device.name) : false;
 }
 
